6e665a25f9d1199a247d6e62b4bea04c2eac7f81
AokiReiko <reiko717@outlook.com>
2017-09-08 08:33:24 +0800

    增加抽屉布局；增加标签滑动；图标更新；

--- NewsBulletin/app/src/main/java/test/newsbulletin/ItemListActivity.java
+++ NewsBulletin/app/src/main/java/test/newsbulletin/ItemListActivity.java
@@ -5,16 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.TabHost;
 import android.widget.TableLayout;
 import android.widget.TextView;
@@ -22,6 +30,7 @@ import android.widget.TextView;
 
 import test.newsbulletin.dummy.DummyContent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -39,21 +48,30 @@ public class ItemListActivity extends AppCompatActivity {
      * device.
      */
     private boolean mTwoPane;
-    static private int lastOffset = 0;
-    static private int lastPosition = 0;
-    View recyclerView;
+    private DrawerLayout mDrawerLayout;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_list);
+        setContentView(R.layout.main_activity);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         toolbar.setTitle(getTitle());
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tablayout);
         setupTabLayout(tabLayout);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout.setupWithViewPager(viewPager);
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+        }
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,9 +81,7 @@ public class ItemListActivity extends AppCompatActivity {
             }
         });
 
-        recyclerView = findViewById(R.id.item_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
+
 
         if (findViewById(R.id.item_detail_container) != null) {
             // The detail container view will be present only in the
@@ -76,14 +92,13 @@ public class ItemListActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        scrollToPosition();
-    }
-    protected void onRestart() {
-        super.onRestart();
-        scrollToPosition();
+    private void setupViewPager(ViewPager viewPager) {
+
+        mAdapter adapter = new mAdapter(this.getSupportFragmentManager());
+        adapter.addFragment(new ItemListFragment(), "Category 1");
+        adapter.addFragment(new Fragment(), "Category 2");
+        adapter.addFragment(new Fragment(), "Category 3");
+        viewPager.setAdapter(adapter);
     }
     private void setupTabLayout(@NonNull TabLayout tabLayout) {
         Log.d("func", "set up tabhost");
@@ -91,7 +106,10 @@ public class ItemListActivity extends AppCompatActivity {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 Log.d("func", "tab Id: " + tab.getPosition());
+                if (tab.getPosition() != 0) {
+                }
             }
+
             @Override
 
             public void onTabUnselected(TabLayout.Tab tab) {
@@ -107,125 +125,35 @@ public class ItemListActivity extends AppCompatActivity {
         });
 
     }
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-
-            private int lastItem = 0;
-            private int firstItem = 0;
-            private int itemSize = 0;
-            private boolean isLoad = false;
 
 
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                itemSize = recyclerView.getAdapter().getItemCount();recyclerView.getLayoutManager().getItemCount();
-                lastItem = ((LinearLayoutManager)recyclerView.getLayoutManager()).findLastVisibleItemPosition();
-                firstItem = ((LinearLayoutManager)recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
-            }
-            @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-                if (lastItem == recyclerView.getAdapter().getItemCount() - 1 && newState == RecyclerView.SCROLL_STATE_IDLE ) {
-                    isLoad = true;
-                    ((SimpleItemRecyclerViewAdapter)recyclerView.getAdapter()).loadMore();
-                }
-                // To update the pre-layout list.
-                recyclerView.getAdapter().notifyDataSetChanged();
-                if(recyclerView.getLayoutManager() != null) {
-                    getPositionAndOffset();
-                }
+    public class mAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
 
-            }
-        });
-    }
-     private void getPositionAndOffset() {
-        LinearLayoutManager layoutManager = (LinearLayoutManager) ((RecyclerView)recyclerView).getLayoutManager();
-        //获取可视的第一个view
-        int topIndex = layoutManager.findFirstVisibleItemPosition();
-        View topView = layoutManager.findViewByPosition(topIndex);
-        if(topView != null) {
-            //获取与该view的顶部的偏移量
-            lastOffset = topView.getTop();
-            //得到该View的数组位置
-            lastPosition = topIndex;
-        }
-    }
-    private void scrollToPosition() {
-        if(((RecyclerView)recyclerView).getLayoutManager() != null && lastPosition >= 0) {
-            ((LinearLayoutManager) ((RecyclerView)recyclerView).getLayoutManager()).scrollToPositionWithOffset(lastPosition, lastOffset);
-        }
-    }
-    public class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
-
-        private final List<DummyContent.DummyItem> mValues;
-
-        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
-            mValues = items;
+        public mAdapter(FragmentManager fm) {
+            super(fm);
         }
 
-        public void loadMore() { DummyContent.loadMore(); }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_list_content, parent, false);
-            return new ViewHolder(view);
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
         }
 
         @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mItem = mValues.get(position);
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
-            //Log.d("func", holder.toString()+"-"+position + "-" + holder.mContentView.getText());
-            holder.mView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mTwoPane) {
-                        Bundle arguments = new Bundle();
-                        arguments.putString(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-                        ItemDetailFragment fragment = new ItemDetailFragment();
-                        fragment.setArguments(arguments);
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.item_detail_container, fragment)
-                                .commit();
-                    } else {
-                        Context context = v.getContext();
-                        Intent intent = new Intent(context, ItemDetailActivity.class);
-                        intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-
-                        context.startActivity(intent);
-                    }
-                }
-            });
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
         }
 
         @Override
-        public int getItemCount() {
-            return mValues.size();
+        public int getCount() {
+            return mFragments.size();
         }
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public final View mView;
-            public final TextView mIdView;
-            public final TextView mContentView;
-            public DummyContent.DummyItem mItem;
-
-            public ViewHolder(View view) {
-                super(view);
-                mView = view;
-                mIdView = (TextView) view.findViewById(R.id.id);
-                mContentView = (TextView) view.findViewById(R.id.content);
-            }
-
-            @Override
-            public String toString() {
-                return super.toString() + " '" + mContentView.getText() + "'";
-            }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
         }
     }
+
 }
---
+++ NewsBulletin/app/src/main/java/test/newsbulletin/ItemListFragment.java
@@ -0,0 +1,193 @@
+package test.newsbulletin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+import test.newsbulletin.dummy.DummyContent;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ItemListFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ItemListFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ItemListFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    static private int lastOffset = 0;
+    static private int lastPosition = 0;
+    View recyclerView;
+    // TODO: Rename and change types of parameters
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("func", "oncreate");
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.d("func", "oncreate view");
+        RecyclerView rv = (RecyclerView) inflater.inflate(
+                R.layout.item_list, container, false);
+        setupRecyclerView(rv);
+        recyclerView = rv;
+        return rv;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        scrollToPosition();
+    }
+
+    private void scrollToPosition() {
+        if(((RecyclerView)recyclerView).getLayoutManager() != null && lastPosition >= 0) {
+            ((LinearLayoutManager) ((RecyclerView)recyclerView).getLayoutManager()).scrollToPositionWithOffset(lastPosition, lastOffset);
+        }
+    }
+    private void getPositionAndOffset() {
+        LinearLayoutManager layoutManager = (LinearLayoutManager) ((RecyclerView)recyclerView).getLayoutManager();
+        //获取可视的第一个view
+        int topIndex = layoutManager.findFirstVisibleItemPosition();
+        View topView = layoutManager.findViewByPosition(topIndex);
+        if(topView != null) {
+            //获取与该view的顶部的偏移量
+            lastOffset = topView.getTop();
+            //得到该View的数组位置
+            lastPosition = topIndex;
+        }
+    }
+
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+        recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+
+            private int lastItem = 0;
+            private int firstItem = 0;
+            private int itemSize = 0;
+            private boolean isLoad = false;
+
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                itemSize = recyclerView.getAdapter().getItemCount();recyclerView.getLayoutManager().getItemCount();
+                lastItem = ((LinearLayoutManager)recyclerView.getLayoutManager()).findLastVisibleItemPosition();
+                firstItem = ((LinearLayoutManager)recyclerView.getLayoutManager()).findFirstVisibleItemPosition();
+            }
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if (lastItem == recyclerView.getAdapter().getItemCount() - 1 && newState == RecyclerView.SCROLL_STATE_IDLE ) {
+                    isLoad = true;
+                    ((SimpleItemRecyclerViewAdapter)recyclerView.getAdapter()).loadMore();
+                }
+                // To update the pre-layout list.
+                recyclerView.getAdapter().notifyDataSetChanged();
+                if(recyclerView.getLayoutManager() != null) {
+                    getPositionAndOffset();
+                }
+
+            }
+        });
+    }
+    public class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final List<DummyContent.DummyItem> mValues;
+
+        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+            mValues = items;
+        }
+
+        public void loadMore() { DummyContent.loadMore(); }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mItem = mValues.get(position);
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+            //Log.d("func", holder.toString()+"-"+position + "-" + holder.mContentView.getText());
+            holder.mView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Context context = v.getContext();
+                    Intent intent = new Intent(context, ItemDetailActivity.class);
+                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+
+                    context.startActivity(intent);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final TextView mIdView;
+            public final TextView mContentView;
+            public DummyContent.DummyItem mItem;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mIdView = (TextView) view.findViewById(R.id.id);
+                mContentView = (TextView) view.findViewById(R.id.content);
+            }
+
+            @Override
+            public String toString() {
+                return super.toString() + " '" + mContentView.getText() + "'";
+            }
+        }
+    }
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
